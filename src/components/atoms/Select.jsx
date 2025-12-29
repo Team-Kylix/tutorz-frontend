5658db045b44d8e04968e619b9db86babf79ee6f
vinod warnakulasooriya <116092558+vinuwa98@users.noreply.github.com>
2025-12-29 23:46:23 +0530

    Complete atoms for theme

--- src/components/atoms/Select.jsx
+++ src/components/atoms/Select.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
-const Select = ({ children, ...props }) => {
+const Select = ({ children, className = '', ...props }) => {
     return (
-        <select {...props}>
+        <select 
+            className={`
+                w-full px-3 py-2 border rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500
+                bg-white dark:bg-gray-800 
+                border-gray-300 dark:border-gray-700 
+                text-gray-900 dark:text-white 
+                ${className}
+            `.trim()}
+            {...props}
+        >
             {children}
         </select>
     );
 };
-
 
 export default Select;
