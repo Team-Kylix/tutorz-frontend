09b397e21553a66da0369b0807039b44db4cbd54
vinod warnakulasooriya <116092558+vinuwa98@users.noreply.github.com>
2026-05-23 10:23:07 +0530

    Bump app version to 0.4.5
    Update project version from 0.4.4 to 0.4.5. Adjusted package.json and package-lock.json, and updated APP_VERSION in src/config/version.js to '0.4.5-beta' to reflect the new release.

--- src/config/version.js
+++ src/config/version.js
@@ -14,5 +14,5 @@
 //   4. (Optional) Update MinTokenDate in DB via SQL to force API-level logout
 // ============================================================
 
-export const APP_VERSION = '0.4.4-beta';
+export const APP_VERSION = '0.4.5-beta';
 
